8bc926a52998a69642935d72fb761d5c1fb73684
pavils.kuzmins <pavils.kuzmins@gmail.com>
2020-11-20 23:18:45 +0200

    eslint modified brain-even.js and brain-even-logic.js

--- src/brain-even-logic.js
+++ src/brain-even-logic.js
@@ -1,39 +1,32 @@
 import readlineSync from 'readline-sync';
 
-const isNumberEven = (number) => {
-  return (number % 2 === 0) ? 'yes' : 'no';
-}
+const isNumberEven = (number) => ((number % 2 === 0) ? 'yes' : 'no');
 
-const getRandomNumber = (min, max) => {
-    return Math.random() * (max - min) + min;
-}
+const getRandomNumber = (min, max) => Math.random() * (max - min) + min;
 
-export default  () =>  {
+export default () => {
+  let rightAnswerCount = 0;
 
-    let rightAnswerCount = 0;
+  console.log('Welcome to the Brain Games!');
 
-    console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name?: ');
 
-    let name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}!`);
 
-    console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  while (rightAnswerCount < 3) {
+    const randomNumber = Math.round(getRandomNumber(1, 100));
 
-    while (rightAnswerCount < 3) {
+    const getAnswer = readlineSync.question(`Question: ${randomNumber}: `);
 
-        let randomNumber = Math.round(getRandomNumber(1, 100));
-
-        let getAnswer = readlineSync.question(`Question: ${randomNumber}: `);
-
-        if (getAnswer === isNumberEven(randomNumber)) {
-            console.log('Correct!');
-            rightAnswerCount++;
-        } else {
-            return console.log(`${getAnswer} is wrong answer ;(. Correct answer was ${isNumberEven(randomNumber)}.
+    if (getAnswer === isNumberEven(randomNumber)) {
+      console.log('Correct!');
+      rightAnswerCount++;
+    } else {
+      return console.log(`${getAnswer} is wrong answer ;(. Correct answer was ${isNumberEven(randomNumber)}.
                 Let's try again, ${name}!`);
-                
-        }
     }
-    return console.log(`Congratulations, ${name}!`);
-}
+  }
+  return console.log(`Congratulations, ${name}!`);
+};
