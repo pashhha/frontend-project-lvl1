95de6c59b52375f27f0590f0005b5e9467bc7ca3
pavils.kuzmins <pavils.kuzmins@gmail.com>
2020-11-29 22:56:14 +0200

    improved index.js braincalc-logic.js

--- src/games/braincalc-logic.js
+++ src/games/braincalc-logic.js
@@ -1,26 +1,33 @@
 import { getRandomNumber } from './braineven-logic.js';
 
-export const getRandomArrayValue = (array) => {
-  return array[Math.floor(Math.random() * array.length)];
-};
+export const getRandomArrayValue = (array) => array[Math.floor(Math.random() * array.length)];
 
 export const brainCalc = {
-  rules: "What is the result of the expression?",
+  rules: 'What is the result of the expression?',
   question() {
     const operators = ['+', '-', '*'];
     let number1;
     let number2;
 
     const operator = getRandomArrayValue(operators);
-    if(operator === '*') {
-      number1 = getRandomNumber(1, 10);
-      number2 = getRandomNumber(1, 10);
-    } else {
-    number1 = getRandomNumber(31, 70);
-    number2 = getRandomNumber(1, 30);
-    };
-    let answer = [number1, number2, operator];
-    brainCalc.correctAnswer = eval(answer[0] + answer[2] + answer[1]);
-    return `${number1} ${operator} ${number2} =` ;
+    switch (operator) {
+      case '*':
+        number1 = getRandomNumber(2, 10);
+        number2 = getRandomNumber(2, 10);
+        brainCalc.correctAnswer = number1 * number2;
+        break;
+      case '+':
+        number1 = getRandomNumber(10, 30);
+        number2 = getRandomNumber(1, 20);
+        brainCalc.correctAnswer = number1 + number2;
+        break;
+      case '-':
+        number1 = getRandomNumber(30, 70);
+        number2 = getRandomNumber(1, 30);
+        brainCalc.correctAnswer = number1 - number2;
+        break;
+      default: break;
+    }
+    return `${number1} ${operator} ${number2} =`;
   },
 };
